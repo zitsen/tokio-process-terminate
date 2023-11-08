e781ba9d47c73330bee1b11333e63c03016328dc
Huo Linhe <linhehuo@gmail.com>
2023-11-08 15:40:57 +0800

    docs: refine docs

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # tokio-process-terminate
+
+<!-- cargo-sync-readme start -->
+
+<!-- cargo-sync-readme -->
+Extensions to `tokio::process::Child` to terminate processes.
+
+```rust
+use tokio::process::Command;
+use tokio_process_terminate::TerminateExt;
+
+#[tokio::main]
+async fn main() {
+    let mut command = Command::new("sleep")
+        .arg("10")
+        .spawn()
+        .unwrap();
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    let exit = command.terminate_wait().await.unwrap();
+    dbg!(exit);
+    let code = exit.code();
+    // On Unix, code should be `None` if the process was terminated by a signal.
+    assert!(code.is_none());
+}
+```
+
+<!-- cargo-sync-readme end -->
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! <!-- cargo-sync-readme -->
+//! Extensions to `tokio::process::Child` to terminate processes.
+//!
+//! ```rust
+//! use tokio::process::Command;
+//! use tokio_process_terminate::TerminateExt;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let mut command = Command::new("sleep")
+//!         .arg("10")
+//!         .spawn()
+//!         .unwrap();
+//!     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+//!     let exit = command.terminate_wait().await.unwrap();
+//!     dbg!(exit);
+//!     let code = exit.code();
+//!     // On Unix, code should be `None` if the process was terminated by a signal.
+//!     assert!(code.is_none());
+//! }
+//! ```
+
 use std::{process::ExitStatus, time::Duration};
 
 #[async_trait::async_trait]
@@ -20,6 +42,9 @@ pub trait TerminateExt {
     }
 
     /// Terminate the process and wait for it to exit, or kill it after a timeout.
+    ///
+    /// If the process exits before the timeout, the exit status is returned.
+    /// If the timeout elapses before the process exits, it is killed and `None` is returned.
     async fn terminate_timeout(
         &mut self,
         timeout: Duration,
